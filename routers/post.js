8a777d6d68fdba9e0b71027dd07de734425c7112
FrancescoCernuto <cernutofrancesco2@gmail.com>
2024-11-20 16:33:56 +0100

    add-CRUD

---
+++ routers/post.js
@@ -0,0 +1,34 @@
+const express = require("express")
+const router = express.Router();
+
+module.exports = router;
+
+/* Index */
+router.get("/", (req, res) => {
+    res.send("Lista dei post");
+  });
+  
+  /* Show */
+  router.get("/:view", (req, res) => {
+    res.send("Visualizza il post");
+  });
+  
+  /* Create */
+  router.post("/", (req, res) => {
+    res.send("Crea il post");
+  });
+  
+  /* Update */
+  router.put("/:update", (req, res) => {
+    res.send("Modifica completamente il post");
+  });
+  
+  /* Modify */
+  router.patch("/:modify", (req, res) => {
+    res.send("Modifica parzialmente il post");
+  });
+  
+  /* Delete */
+  router.delete("/destroy", (req, res) => {
+    res.send("Cancellazione del post");
+  });
